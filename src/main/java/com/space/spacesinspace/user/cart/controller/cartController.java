1d9d9424815118277f6f09a9178621c654bb21ca
KimHoDong <tunys123@gmail.com>
2024-09-20 13:04:29 +0900

    fix: 결제전체조회, 결제선택조회 수정

---
+++ src/main/java/com/space/spacesinspace/user/cart/controller/cartController.java
@@ -0,0 +1,15 @@
+package com.space.spacesinspace.user.cart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user/cart/*")
+public class cartController {
+
+    @GetMapping("cartList")
+    public String cart(){}
+
+
+}
