51a3a763fa4cc378f82b4e5a17f2e099a40d8797
scho123 <nvmsnach@gmail.com>
2024-09-20 15:03:54 +0900

    fix: spring security config
    login config 수정; 사용자와 관리자 모두 admin dashboard 로 이동되는 버그 수정

--- src/main/java/com/space/spacesinspace/config/SecurityConfig.java
+++ src/main/java/com/space/spacesinspace/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.space.spacesinspace.config;
 
 import com.space.spacesinspace.exception.AuthFailHandler;
+import com.space.spacesinspace.exception.AuthSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
@@ -18,10 +19,12 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SecurityConfig {
 
     private AuthFailHandler authFailHandler;
+    private AuthSuccessHandler authSuccessHandler;
 
     @Autowired
-    public SecurityConfig(AuthFailHandler authFailHandler) {
+    public SecurityConfig(AuthFailHandler authFailHandler, AuthSuccessHandler authSuccessHandler) {
         this.authFailHandler = authFailHandler;
+        this.authSuccessHandler = authSuccessHandler;
     }
 
     @Bean
@@ -45,14 +48,16 @@ public class SecurityConfig {
             login.loginPage("/auth/login");
             login.usernameParameter("memberId");
             login.passwordParameter("memberPw");
-            login.defaultSuccessUrl("/", true);
+//            login.defaultSuccessUrl("/", true)
+            login.successHandler(authSuccessHandler);
             login.failureHandler(authFailHandler);
         }).formLogin(login -> {
-            login.loginPage("/auth/admin/login")  // Separate login page for admin
-                    .usernameParameter("memberId")
-                    .passwordParameter("memberPw")
-                    .defaultSuccessUrl("/admin/member/selectAllMembers", true)  // Admin dashboard
-                    .failureHandler(authFailHandler);
+            login.loginPage("/auth/admin/login");  // Separate login page for admin
+            login.usernameParameter("memberId");
+            login.passwordParameter("memberPw");
+//            login.defaultSuccessUrl("/admin/member/selectAllMembers", true)  // Admin dashboard
+            login.successHandler(authSuccessHandler);
+            login.failureHandler(authFailHandler);
         }).logout(logout -> {
             logout.logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout"));
             logout.deleteCookies("JSESSIONID");
