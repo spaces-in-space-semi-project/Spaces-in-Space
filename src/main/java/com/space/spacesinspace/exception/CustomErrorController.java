fa62cccf3cdfe01ba9f444b22be937373f11f9be
scho123 <nvmsnach@gmail.com>
2024-10-04 00:58:15 +0900

    feat: error page, main carousel pic change

--- src/main/java/com/space/spacesinspace/exception/CustomErrorController.java
+++ src/main/java/com/space/spacesinspace/exception/CustomErrorController.java
@@ -1,40 +1,40 @@
-//package com.space.spacesinspace.exception;
-//
-//import jakarta.servlet.http.HttpServletRequest;
-//import org.springframework.boot.web.error.ErrorAttributeOptions;
-//import org.springframework.boot.web.servlet.error.ErrorAttributes;
-//import org.springframework.boot.web.servlet.error.ErrorController;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.context.request.WebRequest;
-//
-//import java.util.Map;
-//
-//@Controller
-//public class CustomErrorController implements ErrorController {
-//
-//    private final ErrorAttributes errorAttributes;
-//
-//    public CustomErrorController(ErrorAttributes errorAttributes) {
-//        this.errorAttributes = errorAttributes;
-//    }
-//
-//    @RequestMapping("/error")
-//    public String handleError(HttpServletRequest request, Model model) {
-//        Map<String, Object> errorDetails = errorAttributes.getErrorAttributes((WebRequest) request, ErrorAttributeOptions.defaults());
-//        Integer statusCode = (Integer) errorDetails.get("status");
-//
-//        if (statusCode != null) {
-//            if (statusCode == HttpStatus.NOT_FOUND.value()) {
-//                return "error-404";  // 404 페이지로 이동
-//            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-//                return "error-500";  // 500 페이지로 이동
-//            }
-//        }
-//
-//        // 그 외 에러 처리
-//        return "error-500";
-//    }
-//}
+package com.space.spacesinspace.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    private final ErrorAttributes errorAttributes;
+
+    public CustomErrorController(ErrorAttributes errorAttributes) {
+        this.errorAttributes = errorAttributes;
+    }
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        Map<String, Object> errorDetails = errorAttributes.getErrorAttributes((WebRequest) request, ErrorAttributeOptions.defaults());
+        Integer statusCode = (Integer) errorDetails.get("status");
+
+        if (statusCode != null) {
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error-404";  // 404 페이지로 이동
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "error-500";  // 500 페이지로 이동
+            }
+        }
+
+        // 그 외 에러 처리
+        return "error-500";
+    }
+}
--- src/main/java/com/space/spacesinspace/exception/ErrorController.java
+++ src/main/java/com/space/spacesinspace/exception/ErrorController.java
@@ -1,39 +1,39 @@
-//package com.space.spacesinspace.exception;
-//
-//import jakarta.servlet.http.HttpServletRequest;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//public class ErrorController {
-//
-//    @GetMapping("/error-404")
-//    public String notFound() {
-//        return "error-404";
-//    }
-//
-//    @GetMapping("/error-500")
-//    public String serverError() {
-//        return "error-500";
-//    }
-//
-//    @GetMapping("/error-400")
-//    public String badRequest() {
-//        return "error-500";
-//    }
-//
-//    @GetMapping("/error-403")
-//    public String forbidden() {
-//        return "error-500";
-//    }
-//
-//    // 405 에러 처리 (허용되지 않은 메서드)
-//    @GetMapping("/error-405")
-//    public String methodNotAllowedError() {
-//        return "error-500";
-//    }
-//
-//}
-//
+package com.space.spacesinspace.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorController {
+
+    @GetMapping("/error-404")
+    public String notFound() {
+        return "error-404";
+    }
+
+    @GetMapping("/error-500")
+    public String serverError() {
+        return "error-500";
+    }
+
+    @GetMapping("/error-400")
+    public String badRequest() {
+        return "error-500";
+    }
+
+    @GetMapping("/error-403")
+    public String forbidden() {
+        return "error-500";
+    }
+
+    // 405 에러 처리 (허용되지 않은 메서드)
+    @GetMapping("/error-405")
+    public String methodNotAllowedError() {
+        return "error-500";
+    }
+
+}
+
